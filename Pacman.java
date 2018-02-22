b9b3f1b845de6f4e9ea93527a1beeb7422fdf956
ArchonDWSP <36726187+ArchonDWSP@users.noreply.github.com>
2018-02-22 11:21:26 +0100

    Update Pacman.java

--- Pacman.java
+++ Pacman.java
@@ -12,7 +12,7 @@ public class DrawRectangle {
 		// create a basic JFrame
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		JFrame frame = new JFrame("Pacman GLPOO");
-		frame.setSize(1080, 720);
+		frame.setSize(1920, 1080);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// add panel to main frame
@@ -27,32 +27,94 @@ public class DrawRectangle {
 class MyPanel extends JPanel {
 	public void paint(Graphics g) {
 		
-		g.setColor(Color.yellow);
-		g.fillArc(0, 50, 150, 150, 45, 265);
-		g.setColor(Color.white);
-		g.fillOval(200, 120, 20, 20);
+		g.setColor(Color.yellow); // Pacman
+		g.fillArc(60, 50, 150, 150, 45, 265);
+		
+		g.setColor(Color.white); 
+		g.fillOval(200, 120, 20, 20); // Balls couloir horizontal haut
 		g.fillOval(400, 120, 20, 20);
 		g.fillOval(600, 120, 20, 20);
-		g.fillOval(800, 105, 50, 50);
-		g.setColor(Color.blue);
-		g.fillRect(0, 250, 700, 30);
-		g.fillRect(700, 250, 30, 700);
-		g.fillRect(950, 0, 30, 950);
-		g.fillRect(0, 0, 950, 30);
-		g.setColor(Color.red);
-		g.fillArc(765, 500, 150, 150, 0, 180);
-		g.fillRect(765, 575, 150, 40);
-		g.fillOval(765, 595, 44, 40);
-		g.fillOval(800, 595, 44, 40);
-		g.fillOval(835, 595, 44, 40);
-		g.fillOval(870, 595, 44, 40);
-		g.setColor(Color.white);
-		g.fillOval(780, 540, 45, 45);
-		g.fillOval(855, 540, 45, 45);
-		g.setColor(Color.gray);
-		g.fillOval(780, 550, 25, 25);
-		g.fillOval(855, 550, 25, 25);
+		g.fillOval(800, 120, 20, 20);
+		g.fillOval(1000, 120, 20, 20);
+		g.fillOval(1200, 120, 20, 20);
+		g.fillOval(1400, 120, 20, 20);
+		g.fillOval(1600, 120, 20, 20);
+		g.fillOval(1740, 105, 50, 50);
+		
+		g.fillOval(200, 890, 20, 20); // balls couloir horizontal bas
+		g.fillOval(400, 890, 20, 20);
+		g.fillOval(600, 890, 20, 20);
+		g.fillOval(800, 890, 20, 20);
+		g.fillOval(1000, 890, 20, 20);
+		g.fillOval(1200, 890, 20, 20);
+		g.fillOval(1400, 890, 20, 20);
+		g.fillOval(1600, 890, 20, 20);
+		
+		
+		
+		
+		g.setColor(Color.blue); // Walls
+		g.fillRect(0, 0, 2000, 30); // mur horizontal du haut
+		g.fillRect(0, 250, 30, 800); // premier mur vertical
+		g.fillRect(250, 250, 30, 530); // second mur vertical
+		g.fillRect(250, 250, 1400, 30); // mur horizontal milieu haut
+		g.fillRect(1630, 250, 30, 530); // troisieme mur vertical
+		g.fillRect(1880, 0, 30, 780); // quatrième mur vertical
+		g.fillRect(250, 750, 1400, 30); // mur horizontal milieu bas
+		
+		g.fillRect(0, 1020, 2000, 30); // mur horizontal du bas
+		
+		
+		g.setColor(Color.red); // Phantom rouge
+		g.fillArc(1700, 500, 150, 150, 0, 180);
+		g.fillRect(1700, 575, 150, 40);
+		g.fillOval(1700, 595, 44, 40);
+		g.fillOval(1735, 595, 44, 40);
+		g.fillOval(1770, 595, 44, 40);
+		g.fillOval(1805, 595, 44, 40);
+		
+		g.setColor(Color.white); // Eyes
+		g.fillOval(1720, 540, 45, 45);
+		g.fillOval(1785, 540, 45, 45);
+		
+		g.setColor(Color.gray); // Pupils
+		g.fillOval(1720, 550, 25, 25);
+		g.fillOval(1785, 550, 25, 25);
+		
+		
+		g.setColor(Color.pink); // Phantom rose
+		g.fillArc(1400, 830, 150, 150, 0, 180);
+		g.fillRect(1400, 905, 150, 40);
+		g.fillOval(1400, 925, 44, 40);
+		g.fillOval(1435, 925, 44, 40);
+		g.fillOval(1470, 925, 44, 40);
+		g.fillOval(1505, 925, 44, 40);
+		
+		g.setColor(Color.white); // Eyes
+		g.fillOval(1420, 870, 45, 45);
+		g.fillOval(1485, 870, 45, 45);
+		
+		g.setColor(Color.gray); // Pupils
+		g.fillOval(1420, 880, 25, 25);
+		g.fillOval(1485, 880, 25, 25);
+		
+		
+		g.setColor(Color.cyan); // Phantom cyan
+		g.fillArc(1830, 830, 150, 150, 0, 180);
+		g.fillRect(1830, 905, 150, 40);
+		g.fillOval(1830, 925, 44, 40);
+		g.fillOval(1865, 925, 44, 40);
+		g.fillOval(1900, 925, 44, 40);
+		g.fillOval(1935, 925, 44, 40);
+		
+		g.setColor(Color.white); // Eyes
+		g.fillOval(1850, 870, 45, 45);
+		g.fillOval(1915, 870, 45, 45);
+		
+		g.setColor(Color.gray); // Pupils
+		g.fillOval(1850, 880, 25, 25);
+		g.fillOval(1915, 880, 25, 25);
+		
 
 	}
 }
-
